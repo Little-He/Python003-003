bba8e2b21d9fa68ab4048dfee717e6f003f1d347
hepenghui <hepenghui@kuaiyong.net>
2020-08-22 18:44:41 +0800

    作业

--- week01/class2/maoyan/maoyan/spiders/maoyanmovie.py
+++ week01/class2/maoyan/maoyan/spiders/maoyanmovie.py
@@ -14,7 +14,7 @@ class MaoyanmovieSpider(scrapy.Spider):
         # url = "https://maoyan.com/films?showType=3"
         # response = requests.get(url=url)
         # print(response.status_code, response.text)
-        # Selector(response=response).xpath("")
+        # movie_hover_title_list = Selector(text=f.read()).xpath('//div[@class="movie-item-hover"]')
 
 
         # 本地调试
@@ -28,8 +28,8 @@ class MaoyanmovieSpider(scrapy.Spider):
             if i > 9:
                 break
             movie_name = movie_hover_title.xpath("./a/div/div[1]/span/text()").extract_first()
-            movie_type = movie_hover_title.xpath("./a/div/div[2]/text()")[1].extract().strip()
-            movie_date = movie_hover_title.xpath("./a/div/div[4]/text()")[1].extract().strip()
+            movie_type = movie_hover_title.xpath("./a/div/div[2]/text()").extract()[1].strip()
+            movie_date = movie_hover_title.xpath("./a/div/div[4]/text()").extract()[1].strip()
             items.append({"movie_name":movie_name,"movie_type" : movie_type,"movie_date" : movie_date})
             i += 1
         return items
