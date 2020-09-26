7911adf746a570567ba363df52363f89afa74f4e
hepenghui <hepenghui@kuaiyong.net>
2020-09-26 20:54:29 +0800

    add myDjango

---
+++ week05/myDjango/douban/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#-*- coding:utf8 -*-
+# @TIME     :  8:28 下午
+# @FILE     : urls.py
+from django.urls import path,re_path
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path("<str:comment>/<int:id>.html",views.comment)
+]
