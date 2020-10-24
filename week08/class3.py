6e97d7e3b5d651fe02c9a3195f3848d0e96108d3
hepenghui <hepenghui@chehang168.com>
2020-10-24 10:16:02 +0800

    class1

--- week08/class3.py
+++ week08/class3.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 import random
 import time
+from functools import wraps
 
 def timer(func):
-    def innser(*args,**kwargs) :
+    @wraps(func)
+    def innser(*args,**kwargs):
         stime = time.time()
         res = func(*args,**kwargs)
         etime = time.time()
@@ -13,15 +15,18 @@ def timer(func):
     return innser
 
 class Timer(object):
-    def __init__(self,func):
-        self.__func = func
+    def __init__(self,*args, **kwargs):
+        super(Timer,self).__init__(*args,**kwargs)
 
-    def __call__(self, *args, **kwargs):
-        stime = time.time()
-        res = self.__func(*args, **kwargs)
-        etime = time.time()
-        print(f'{self.__func.__name__} run {etime - stime} s')
-        return res
+    def __call__(self, func):
+        @wraps(func)
+        def innser(*args, **kwargs):
+            stime = time.time()
+            res = func(*args, **kwargs)
+            etime = time.time()
+            print(f'{func.__name__} run {etime - stime} s')
+            return res
+        return innser
 
 
 @timer
@@ -31,11 +36,13 @@ def func():
     print(f"rest {s} s")
 
 func()
+print(func.__name__)
 
-@Timer
+@Timer()
 def func1():
     s = random.randint(1, 5)
     time.sleep(s)
     print(f"rest {s} s")
 
 func1()
+print(func1.__name__)
